937a8f205f6067396273daaead4383af6f1f3841
若羽 <dydhyh@foxmail.com>
2020-12-16 22:04:36 +0800

    refactor: add import entry.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import RouteGenerator from './generator';
+import { RouteName } from './decorators/RouteName';
+import { Context, EnableProp } from './decorators/Context';
+import { Alias } from './decorators/Alias';
+import { Meta } from './decorators/Meta';
 
 export default RouteGenerator;
+export { RouteName, Context, EnableProp, Alias, Meta };
