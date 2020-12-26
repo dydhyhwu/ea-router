9a892a963361b018276cd0676945c4cca7ad1501
若羽 <dydhyh@foxmail.com>
2020-12-26 17:10:13 +0800

    feat: add type denfine file

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+declare module 'ea-router' {
+    export default class RouteGenerate {
+        constructor(dir: any);
+        setDefaultLayout(layout: any): void;
+        setPageNotFound(layout: any): void;
+        generate(): Array<any>;
+    }
+    export function RouteName(name: string): any;
+    export function Context(...params: string[]): any;
+    export function EnableProp(): any;
+    export function Alias(alias: string): any;
+    export function Meta(data: any): any;
+}
