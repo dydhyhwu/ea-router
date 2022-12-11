9bb20d374c2f443e7e552c66ac61385141a9fb20
赵晏龙 <kitten63928@live.com>
2022-12-11 23:30:31 +0800

    feat:完成vue3example的title部分

--- examples/vue3/src/App.vue
+++ examples/vue3/src/App.vue
@@ -5,16 +5,8 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <div class="title">ea-router vue3 example</div>
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -30,18 +22,29 @@ import HelloWorld from "./components/HelloWorld.vue";
 header {
   line-height: 1.5;
   max-height: 100vh;
+  display: flex;
+  place-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header .wrapper {
+  display: flex;
+  place-items: flex-start;
+  align-items: center;
+  width: 100%;
+  background: skyblue;
+}
+
+header .title {
+  flex-grow: 1;
+  font-size: 2rem;
+  padding-left: 1rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  text-align: left;
+  font-size: 1rem;
+
+  padding: 1rem;
 }
 
 nav a.router-link-exact-active {
@@ -60,32 +63,5 @@ nav a {
 
 nav a:first-of-type {
   border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
